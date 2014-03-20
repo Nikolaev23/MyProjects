d68f5fddfbdfb645e4e46ea121ad86c73110f118
Nikolaev23 <st3851ba@gmail.com>
2014-03-20 23:33:45 +0200

    .init

---
+++ Exams/SampleExam/03.ForrestRoad/ForrestRoad.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class ForrestRoad
+{
+    static void Main()
+    {
+        int width = int.Parse(Console.ReadLine());
+        int height = (2 * width) - 1;
+        
+        for (int row = 1; row <= width; row++)
+        {
+            for (int col = 1; col <= width; col++)
+            {
+                if (row == col)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+
+            Console.WriteLine();
+        }
+        for (int row = width - 1; row > 0; row--)
+        {
+            for (int col = 1; col <= width; col++)
+            {
+                if (row != col)
+                {
+                    Console.Write(".");
+                }
+                else
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
